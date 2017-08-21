73b98a1a257679a2d8d93916c08576933264a6e5
Jack Works <zjwpeter@gmail.com>
2017-08-21 14:31:05 +0800

    Fix typescript def file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,20 @@
-import React from 'react';
-export default class MarkdownEditor extends React.Component<{
-	initialContent: string
-	iconsSet: 'materialize-ui' | 'font-awesome'
-	onContentChange?: (content: string) => void
-	styles?: {
-		styleMarkdownEditorHeader?: React.CSSProperties
-		styleMarkdownEditorContainer?: React.CSSProperties
-		styleMarkdownMenu?: React.CSSProperties
-		styleMarkdownTextArea?: React.CSSProperties
-		styleMarkdownPreviewArea?: React.CSSProperties
-		styleMarkdownEditorTabs?: React.CSSProperties
-		styleTab?: React.CSSProperties
-		styleActiveTab?: React.CSSProperties
-	}
-	previewClass?: string
-	textareaClass?: string
-}> { }
+declare module 'react-markdown-editor' {
+	import React from 'react';
+	export default class MarkdownEditor extends React.Component<{
+		initialContent: string
+		iconsSet: 'materialize-ui' | 'font-awesome'
+		onContentChange?: (content: string) => void
+		styles?: {
+			styleMarkdownEditorHeader?: React.CSSProperties
+			styleMarkdownEditorContainer?: React.CSSProperties
+			styleMarkdownMenu?: React.CSSProperties
+			styleMarkdownTextArea?: React.CSSProperties
+			styleMarkdownPreviewArea?: React.CSSProperties
+			styleMarkdownEditorTabs?: React.CSSProperties
+			styleTab?: React.CSSProperties
+			styleActiveTab?: React.CSSProperties
+		}
+		previewClass?: string
+		textareaClass?: string
+	}> { }
+}
