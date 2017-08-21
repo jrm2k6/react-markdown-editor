04e908b0a740577afdc3faeb665345799e318ee1
Jack Works <zjwpeter@gmail.com>
2017-08-21 13:07:18 +0800

    Fix test error

--- __tests__/markdownEditor-test.js
+++ __tests__/markdownEditor-test.js
@@ -1,9 +1,9 @@
 // __tests__/MarkdownEditor-test.js
 
-jest.dontMock('../src/MarkdownEditor.js');
+jest.dontMock('../src/MarkdownEditor');
 jest.dontMock('../src/components/MarkdownEditorMenu');
 jest.dontMock('../src/components/MarkdownEditorTabs');
-jest.dontMock('../src/mixins/ButtonManagerMixin.js');
+jest.dontMock('../src/mixins/ButtonManagerMixin');
 
 var React = require('react');
 var ReactDOM = require('react-dom');
@@ -15,20 +15,20 @@ var MarkdownEditorActions = require('../src/actions/MarkdownEditorActions');
 var PublicMarkdownEditorActions = require('../src/actions/PublicMarkdownEditorActions');
 var enzyme = require('enzyme');
 
-describe('creates markdown editor', function() {
-    it('creates markdown editor element composed of two divs', function() {
+describe('creates markdown editor', function () {
+    it('creates markdown editor element composed of two divs', function () {
         var renderer = TestUtils.createRenderer();
         renderer.render(<MarkdownEditor
-          initialContent="initialContent"
-          iconsSet="font-awesome"/>
+            initialContent="initialContent"
+            iconsSet="font-awesome" />
         );
         var result = renderer.getRenderOutput();
         expect(result.props.children.length).toEqual(2);
     });
 
-    it('markdown editor header is composed of two divs in edit mode', function() {
+    it('markdown editor header is composed of two divs in edit mode', function () {
         var renderer = TestUtils.createRenderer();
-        renderer.render(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        renderer.render(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
 
         var result = renderer.getRenderOutput();
         var menuElements = result.props.children[0];
@@ -41,12 +41,12 @@ describe('creates markdown editor', function() {
         expect(children[1].type.displayName).toEqual('MarkdownEditorTabs');
     });
 
-    it('markdown editor menu has 7 buttons', function() {
-        var editor = enzyme.shallow(<MarkdownEditorMenu iconsSet="font-awesome"/>);
+    it('markdown editor menu has 7 buttons', function () {
+        var editor = enzyme.shallow(<MarkdownEditorMenu iconsSet="font-awesome" />);
         expect(editor.children().length).toEqual(7);
     });
 
-    it('markdown editor tabs element has 2 tabs', function() {
+    it('markdown editor tabs element has 2 tabs', function () {
         var editor = enzyme.shallow(<MarkdownEditorTabs />);
         expect(editor.children().length).toEqual(2);
         var children = editor.children;
@@ -56,25 +56,25 @@ describe('creates markdown editor', function() {
         expect(editor.childAt(1).childAt(0).type()).toEqual('span');
     });
 
-    it('markdown editor content is edit mode by default and displays a text area', function() {
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+    it('markdown editor content is edit mode by default and displays a text area', function () {
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var secondChild = ReactDOM.findDOMNode(editor).children[1];
 
         expect(secondChild.type).toEqual('textarea');
     });
 
-    it('markdown editor content displays initial content on creation', function() {
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+    it('markdown editor content displays initial content on creation', function () {
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var editorTextArea = TestUtils.findRenderedDOMComponentWithTag(editor, 'textarea');
         expect(ReactDOM.findDOMNode(editorTextArea).value).toEqual('initialContent');
     });
 });
 
-describe('toggle preview mode', function() {
-    it('clicking on preview tab changes mode', function() {
+describe('toggle preview mode', function () {
+    it('clicking on preview tab changes mode', function () {
         //given
         MarkdownEditorActions.clickPreviewTab = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var previewTab = TestUtils.scryRenderedDOMComponentsWithClass(editor, 'md-editor-tabs-item')[1];
         var previewTabNode = ReactDOM.findDOMNode(previewTab);
         // when
@@ -85,10 +85,10 @@ describe('toggle preview mode', function() {
         MarkdownEditorActions.clickPreviewTab.mockClear();
     });
 
-    it('clicking on editor tab changes mode', function() {
+    it('clicking on editor tab changes mode', function () {
         //given
         MarkdownEditorActions.clickEditorTab = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var editorTab = TestUtils.scryRenderedDOMComponentsWithClass(editor, 'md-editor-tabs-item')[0];
         var editorTabNode = ReactDOM.findDOMNode(editorTab);
         // when
@@ -99,11 +99,11 @@ describe('toggle preview mode', function() {
     });
 });
 
-describe('menu button interactions', function() {
-    it('clicking on bold button dispatches action', function() {
+describe('menu button interactions', function () {
+    it('clicking on bold button dispatches action', function () {
         //given
         MarkdownEditorActions.makeBold = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'bold-btn');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -114,10 +114,10 @@ describe('menu button interactions', function() {
         expect(MarkdownEditorActions.makeBold).toBeCalled();
     });
 
-    it('clicking on italic button dispatches action', function() {
+    it('clicking on italic button dispatches action', function () {
         //given
         MarkdownEditorActions.makeItalic = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'italic-btn');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -128,10 +128,10 @@ describe('menu button interactions', function() {
         expect(MarkdownEditorActions.makeItalic).toBeCalled();
     });
 
-    it('clicking on header button dispatches action', function() {
+    it('clicking on header button dispatches action', function () {
         //given
         MarkdownEditorActions.makeHeader = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'md-editor-menu-header');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -142,10 +142,10 @@ describe('menu button interactions', function() {
         expect(MarkdownEditorActions.makeHeader).toBeCalled();
     });
 
-    it('clicking on subheader button dispatches action', function() {
+    it('clicking on subheader button dispatches action', function () {
         //given
         MarkdownEditorActions.makeSubHeader = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'md-editor-menu-subheader');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -156,10 +156,10 @@ describe('menu button interactions', function() {
         expect(MarkdownEditorActions.makeSubHeader).toBeCalled();
     });
 
-    it('clicking on list button dispatches action', function() {
+    it('clicking on list button dispatches action', function () {
         //given
         MarkdownEditorActions.makeList = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'list-btn');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -170,10 +170,10 @@ describe('menu button interactions', function() {
         expect(MarkdownEditorActions.makeList).toBeCalled();
     });
 
-    it('clicking on image button dispatches action', function() {
+    it('clicking on image button dispatches action', function () {
         //given
         MarkdownEditorActions.makeImage = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'insert-img-btn');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -184,10 +184,10 @@ describe('menu button interactions', function() {
         expect(MarkdownEditorActions.makeImage).toBeCalled();
     });
 
-    it('clicking on link button dispatches action', function() {
+    it('clicking on link button dispatches action', function () {
         //given
         MarkdownEditorActions.makeLink = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var btn = TestUtils.findRenderedDOMComponentWithClass(editor, 'insert-link-btn');
         var btnNode = ReactDOM.findDOMNode(btn);
 
@@ -197,11 +197,11 @@ describe('menu button interactions', function() {
     });
 });
 
-describe('textarea changes behavior', function() {
-    it('verify typing dispatches an action', function() {
+describe('textarea changes behavior', function () {
+    it('verify typing dispatches an action', function () {
         //given
         PublicMarkdownEditorActions.updateText = jest.fn();
-        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome"/>);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" />);
         var textarea = TestUtils.findRenderedDOMComponentWithClass(editor, 'md-editor-textarea');
         var textareaNode = ReactDOM.findDOMNode(textarea);
 
@@ -212,21 +212,21 @@ describe('textarea changes behavior', function() {
         expect(PublicMarkdownEditorActions.updateText).toBeCalled();
     });
 
-    it('calls onContentChange if provided on each content change', function() {
-      //given
-      var onContentChangeCalled = 0;
-      var _onContentChange = function(content) {
-        onContentChangeCalled++;
-      };
+    it('calls onContentChange if provided on each content change', function () {
+        //given
+        var onContentChangeCalled = 0;
+        var _onContentChange = function (content) {
+            onContentChangeCalled++;
+        };
 
-      var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" onContentChange={_onContentChange}/>);
-      var textarea = TestUtils.findRenderedDOMComponentWithClass(editor, 'md-editor-textarea');
-      var textareaNode = ReactDOM.findDOMNode(textarea);
+        var editor = TestUtils.renderIntoDocument(<MarkdownEditor initialContent="initialContent" iconsSet="font-awesome" onContentChange={_onContentChange} />);
+        var textarea = TestUtils.findRenderedDOMComponentWithClass(editor, 'md-editor-textarea');
+        var textareaNode = ReactDOM.findDOMNode(textarea);
 
-      // when
-      TestUtils.Simulate.change(textareaNode, "markdownContent");
+        // when
+        TestUtils.Simulate.change(textareaNode, "markdownContent");
 
-      // then
-      expect(onContentChangeCalled).toEqual(1);
+        // then
+        expect(onContentChangeCalled).toEqual(1);
     });
 });
